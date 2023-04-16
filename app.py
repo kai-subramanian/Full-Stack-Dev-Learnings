fed5c4d0adc898199d1f455841bd8b9da4278005
kai-subramanian <skailash27082k@gmail.com>
2023-04-16 19:27:52 +0530

    added some more sample data to play around

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, render_template
 
 app = Flask(__name__)
 
@@ -8,6 +8,35 @@ d = {
    "XYZ" : {"Math": 99, "Science": 93, "English": 60},
 }
 
+p ={
+   "COBOL": {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
+   "ALGOL": {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
+   "APL": {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
+   "BASIC": {"name": "BASIC", "publication_year": 1964, "contribution": "runtime interpretation, office tooling"},
+   "PL": {"name": "PL", "publication_year": 1966, "contribution": "constants, function overloading, pointers"},
+   "SIMULA67": {"name": "SIMULA67", "publication_year": 1967,
+                "contribution": "class/object split, subclassing, protected attributes"},
+   "Pascal": {"name": "Pascal", "publication_year": 1970,
+              "contribution": "modern unary, binary, and assignment operator syntax expectations"},
+   "CLU": {"name": "CLU", "publication_year": 1975,
+           "contribution": "iterators, abstract data types, generics, checked exceptions"},
+}
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
 @app.route('/studentMarks')
 def marks():
     return jsonify(d)
+
+@app.route('/studentMarks/<student_name>')
+def show_marks(student_name):
+    if student_name in d:
+        return d[student_name] 
+    else: 
+        return "Not Found"
+    
+@app.route('/programming')
+def programs():
+    return jsonify(p)
--- use.py
+++ use.py
@@ -1,5 +1,8 @@
 import requests as rq
 
+# resp = rq.get('https://reqres.in/api/users?page=2')
 resp = rq.get('http://127.0.0.1:5000/studentMarks')
 print(resp.status_code)
-print(resp.json())
+
+if (resp.status_code == 200):
+    print(resp.json())
